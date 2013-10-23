69aa8af2dfd1dcca9f3b6bd6c76b3b0f40e400ef
OpenShift guest <kparkin203@gmail.com>
2013-10-23 17:42:30 -0400

    second commit for homework

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// JavaScript Document
+
+$(document).ready(function(){
+    
+    var myTodo = [{
+    'name':'Laundry',
+    'complete':true
+    }, {
+        
+    'name': 'Clean the Fridge',
+	'complete': false	
+    }, {
+        
+	'name': 'Grocery Shopping',
+	'complete': true	
+    },{
+        
+	'name': 'Workout',
+	'complete': true	
+    }];
+
+    var html = '<p> To Do List</p>';
+        html += '<ol>';
+
+//document.write('<p>To Do List</p>');
+//document.write('<ul>');
+
+    for(var i = 0; i < myTodo.length; i++) {
+        try {
+         // print(myTasks[i]);
+            html += print(myTodo[i]);
+         } catch (error) {
+    
+            console.log('There is a problem:' + error);
+         }
+    }
+    
+    html+='</ol>';
+    
+    $('.container').append(html);
+});
+    
+//document.write('</ul>');
+
+
+    function print(theTask) {
+	    if(typeof theTask.name === 'undefined') {
+            throw 'The task name is missing!';
+        } else if (typeof theTask.complete === 'undefined') {
+            throw 'The task status is missing!';
+        } 
+			if(theTask.complete) {
+                return '<li class=complete>' + theTask.name + '</li>';
+       		// document.write('<li class="complete">' + theTask.name + '</li>');
+    		} else {
+        		//	document.write('<li>' + theTask.name + '</li>'); 
+                return '<li>' + theTask.name + '</li>';
+         }
+      }
+
+function removeCompleted() {
+    $('.complete').remove();
+}
---
+++ main.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+var myToDo = ['Laundry', 'Cooking', 'Cleaning', 'Grocery', 'Workout'];
+
+var numbers = ['1', '2', '3', '4', '5'];
+
+//Listings all of my tasks for today
+
+document.write('<p>To Do List</p>');
+document.write('<ul>');
+for(var i = 0; i < numbers.length;  i++){
+for(var i = 0; i < myToDo.length;  i++){
+	document.write('<li>' + numbers[i] +' '+ myToDo[i] + '.</li>');
+}
+}
+document.write('</ul>');
